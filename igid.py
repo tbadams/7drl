38ffcec638e1e8d4552e191a7f2148778e433f43
Trevor Adams <trevor.b.adams@gmail.com>
2020-02-29 16:40:41 -0800

    Initial commit, copied from CaveRL.

---
+++ igid.py
@@ -0,0 +1,287 @@
+import tcod as libtcod
+import textwrap
+import math
+ 
+#actual size of the window
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+
+#map size
+MAP_WIDTH = 80
+MAP_HEIGHT = 43
+
+
+# GUI
+PANEL_HEIGHT = 7
+BAR_WIDTH = 20
+PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
+MSG_X = BAR_WIDTH + 2
+MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
+MSG_HEIGHT = PANEL_HEIGHT - 1
+INVENTORY_WIDTH = 50
+CHARACTER_SCREEN_WIDTH = 30
+LEVEL_SCREEN_WIDTH = 40
+
+#fov
+FOV_ALGO = 0  #default FOV algorithm
+FOV_LIGHT_WALLS = True
+MAX_LIGHT_RADIUS = 10
+
+LIMIT_FPS = 20  #20 frames-per-second maximum
+
+#game state strings
+STRING_EXIT = 'exit'
+STRING_NO_ACTION = 'didnt-take-turn'
+STRING_PLAYING = 'playing'
+
+
+#colors
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+color_light_wall = libtcod.Color(130, 110, 50)
+color_light_ground = libtcod.Color(200, 180, 50)
+
+# game state
+game_msgs = []
+# player, inventory
+ 
+class Object:
+    #this is a generic object: the player, a monster, an item, the stairs...
+    #it's always represented by a character on screen.
+    def __init__(self, x, y, char, name, color, blocks=False):
+        self.name = name
+        self.blocks = blocks
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+ 
+    def move(self, dx, dy):
+        #move by the given amount if not blocked
+        if not is_blocked(self.x + dx, self.y + dy):
+            self.x += dx
+            self.y += dy
+ 
+    def draw(self):
+        if libtcod.map_is_in_fov(fov_map, self.x, self.y):
+            #set the color and then draw the character that represents this object at its position
+            libtcod.console_set_default_foreground(con, self.color)
+            libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+ 
+    def clear(self):
+        #erase the character that represents this object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+#map stuff
+class Tile:
+    #a tile of the map and its properties
+    def __init__(self, blocked, block_sight = None):
+        self.explored = False
+        self.blocked = blocked
+ 
+        #by default, if a tile is blocked, it also blocks sight
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+        
+class Rect:
+    #a rectangle on the map. used to characterize a room.
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+        
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+ 
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+        
+def create_room(room):
+    global map
+    #go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+            
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        
+def create_v_tunnel(y1, y2, x):
+    global map
+    #vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+            
+def make_map():
+    global map
+ 
+    #fill map with "blocked" tiles
+    map = [[ Tile(True)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+ 
+    #create two rooms
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+    create_h_tunnel(25, 55, 23)
+    player.x = 25
+    player.y = 23
+    
+#runtime functions
+def render_all():
+    global fov_map, color_dark_wall, color_light_wall
+    global color_dark_ground, color_light_ground
+    global fov_recompute
+    if fov_recompute:
+        #recompute FOV if needed (the player moved or something)
+        fov_recompute = False
+        libtcod.map_compute_fov(fov_map, player.x, player.y, MAX_LIGHT_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+    #draw all objects in the list
+    for object in objects:
+        object.draw()
+    #go through all tiles, and set their background color according to the FOV
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            visible = libtcod.map_is_in_fov(fov_map, x, y)
+            wall = map[x][y].block_sight
+            if not visible:
+                #if it's not visible right now, the player can only see it if it's explored
+                if map[x][y].explored:
+                    #it's out of the player's FOV
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+            else:
+                #it's visible
+                if wall:
+                    libtcod.console_set_char_background(con, x, y, color_light_wall, libtcod.BKGND_SET )
+                else:
+                    libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET )
+                map[x][y].explored = True
+    #blit the contents of "con" to the root console and present it
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+    #print the game messages, one line at a time
+    y = 1
+    for (line, color) in game_msgs:
+        # libtcod.console_set_char_foreground
+        # con.default_fg = color
+        # libtcod.console_print(panel, MSG_X, y, libtcod.BKGND_NONE, line)
+        con.print(MSG_X, y, line, color)
+        y += 1
+
+def message(new_msg, color=libtcod.white):
+    # split the message if necessary, among multiple lines
+    new_msg_lines = textwrap.wrap(new_msg, MSG_WIDTH)
+
+    for line in new_msg_lines:
+        # if the buffer is full, remove the first line to make room for the new one
+        if len(game_msgs) == MSG_HEIGHT:
+            del game_msgs[0]
+
+        # add the new line as a tuple, with the text and the color
+        game_msgs.append((line, color))
+    
+def handle_keys():
+    global fov_recompute
+ 
+    #key = libtcod.console_check_for_keypress()  #real-time
+    key = libtcod.console_wait_for_keypress(True)  #turn-based
+ 
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+ 
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return STRING_EXIT  #exit game
+ 
+    if game_state == STRING_PLAYING: 
+        #movement keys
+        if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+            player.move(0, -1)
+            fov_recompute = True
+     
+        elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+            player.move(0, 1)
+            fov_recompute = True
+     
+        elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+            player.move(-1, 0)
+            fov_recompute = True
+     
+        elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+            player.move(1, 0)
+            fov_recompute = True
+        else:
+            return STRING_NO_ACTION
+        
+def is_blocked(x, y):
+    #first test the map tile
+    if map[x][y].blocked:
+        return True
+ 
+    #now check for any blocking objects
+    for object in objects:
+        if object.blocks and object.x == x and object.y == y:
+            return True
+ 
+    return False
+ 
+ 
+#############################################
+# Initialization & Main Loop
+#############################################
+message('Game begin', libtcod.red)
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, "guess I'll die", False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
+
+#create object representing the player
+player = Object(0, 0, '@', 'player', libtcod.white, blocks=True)
+
+#the list of objects starting with the player
+objects = [player]
+
+#generate map (at this point it's not drawn to the screen)
+make_map()
+
+#generate field of view map based on level map
+fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
+for y in range(MAP_HEIGHT):
+    for x in range(MAP_WIDTH):
+        libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
+ 
+#global variables
+fov_recompute = True
+game_state = STRING_PLAYING
+player_action = None
+
+#main loop
+while not libtcod.console_is_window_closed():
+ 
+    #render the screen
+    render_all()
+    libtcod.console_flush()
+ 
+    #erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
+ 
+    #handle keys and exit game if needed
+    player_action = handle_keys()
+    if player_action == STRING_EXIT:
+        break
