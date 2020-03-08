961253ea0e095a3185cc4bc47c61eeff9508c966
Trevor Adams <trevor.b.adams@gmail.com>
2020-03-08 14:19:17 -0700

    Figure out packaging

--- gid.py
+++ gid.py
@@ -14,13 +14,7 @@ from util import pad
 import os
 import sys
 
-try:
-    user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
-except KeyError:
-    user_paths = []
-print(user_paths)
-
-print(sys.path)
+# print(sys.path)
 
 VERSION = "1.01"
 
